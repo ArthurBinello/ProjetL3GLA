19237a46c133229ac72eb98305fe3a9a80be9d74
Arthur Binello <arthur.binello@gmail.com>
2018-05-20 00:19:53 +0200

    Update googleAPI.php

--- projetGLA/googleAPI.php
+++ projetGLA/googleAPI.php
@@ -24,4 +24,26 @@ function decode_json_to_array($data) {
     return $stack;
 }
 
+function getCoordinates($address){
+	$key = "AIzaSyA2PDrfTTbXNZKOn15K-VbWgLfdTevM3qw";
+	$url = "https://maps.googleapis.com/maps/api/geocode/json?address=".urlencode($address)."&sensor=false&key=".$key;
+	$json = file_get_contents($url);
+    $results = json_decode($json, true);
+
+	$stack = array();
+	$status = $results['status'];
+	if ($status == "OK") {
+        $lat = $results['results']['0']['geometry']['location']['lat'];
+        array_push($stack, $lat);
+        $lng = $results['results']['0']['geometry']['location']['lng'];
+        array_push($stack, $lng);
+    }
+    else{
+    	echo "ERREUR DE GOOGLE PLACES API";
+    }
+
+    print_r($stack);
+	return $stack;
+}
+
 ?>
