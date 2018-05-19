8dccc4262024ab0a463b00d3898be36b075b4923
bygan <ganbyme@gmail.com>
2018-05-19 11:38:13 +0200

    Add files via upload

--- projetGLA/departez.php
+++ projetGLA/departez.php
@@ -1,5 +1,17 @@
-<!doctype html>
-<html>
+<?php
+session_start();
+include 'bd.php';
+include 'index.php';
 
-<body>Departez-vous</body>
-</html>
+$nom=$_POST["nom_0"];
+$adr=$_POST["adr_0"];
+$transport=$_POST["transport"];
+$date=$_POST["date"];
+$heure=$_POST["heure"];
+$minute=$_POST["minute"];
+$duree=$_POST["duree"];
+$preference=$_POST["preference"];
+
+sortir($nom,$adr,$transport,$date,$heure,$minute,$duree, $preference);
+
+?>
