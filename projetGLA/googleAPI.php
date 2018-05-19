5b74bc545f54d543d93dd3f31fdf3323f38e0cbb
Arthur Binello <arthur.binello@gmail.com>
2018-05-20 00:21:54 +0200

    Update googleAPI.php

--- projetGLA/googleAPI.php
+++ projetGLA/googleAPI.php
@@ -9,26 +9,25 @@ function nearbysearch($lat, $lng, $type){
 
 function decode_json_to_array($data) {
 	$stack = array();
-    $results = json_decode($data, true);
-    $status = $results['status'];
-    if ($status == "OK") {
-        foreach($results['results'] as $key => $value)
-		{
+	$results = json_decode($data, true);
+	$status = $results['status'];
+	if ($status == "OK") {
+		foreach($results['results'] as $key => $value) {
 			$var = array($value['id'], $value['geometry']['location']['lat'], $value['geometry']['location']['lng'], $value['name'], $value['vicinity'], $value['rating'], $value['place_id']);
 			array_push($stack, $var);
 		}
-    }
-    else{
-    	echo "ERREUR DE GOOGLE PLACES API";
-    }
-    return $stack;
+	}
+	else{
+		echo "ERREUR DE GOOGLE PLACES API";
+	}
+	return $stack;
 }
 
 function getCoordinates($address){
 	$key = "AIzaSyA2PDrfTTbXNZKOn15K-VbWgLfdTevM3qw";
 	$url = "https://maps.googleapis.com/maps/api/geocode/json?address=".urlencode($address)."&sensor=false&key=".$key;
 	$json = file_get_contents($url);
-    $results = json_decode($json, true);
+	$results = json_decode($json, true);
 
 	$stack = array();
 	$status = $results['status'];
@@ -37,12 +36,11 @@ function getCoordinates($address){
         array_push($stack, $lat);
         $lng = $results['results']['0']['geometry']['location']['lng'];
         array_push($stack, $lng);
-    }
-    else{
-    	echo "ERREUR DE GOOGLE PLACES API";
-    }
-
-    print_r($stack);
+	}
+	else{
+		echo "ERREUR DE GOOGLE GEOCODE API";
+	}
+	
 	return $stack;
 }
 
