8ea5b939c9f7b7a56ff54b6c01b9cf3dd92890f7
colall9414 <35923481+colall9414@users.noreply.github.com>
2018-05-20 23:15:10 +0800

    Add files via upload

--- projetGLA/departez.php
+++ projetGLA/departez.php
@@ -1,10 +1,37 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+ 	<title>Sortie à Paris</title>
+	<body>
 <?php
-session_start();
+
 include 'bd.php';
-include 'index.php';
+
+
+
+//si l'utilisateur sortie avec plusieurs amis et engregister les infos dans le BD
+$i=0;
 
 $nom=$_POST["nom_0"];
 $adr=$_POST["adr_0"];
+while($nom!=''&&$adr!=''){
+	
+	echo $nom;
+	echo $adr;
+	$i++;
+	
+    if((!isset($_POST["nom_".$i]))||(!isset($_POST["adr_".$i]))){
+		break;
+	}else{
+		$nom=$_POST["nom_".$i];
+		$adr=$_POST["adr_".$i];
+	}
+	
+}
+
+
 $transport=$_POST["transport"];
 $date=$_POST["date"];
 $heure=$_POST["heure"];
@@ -12,6 +39,10 @@ $minute=$_POST["minute"];
 $duree=$_POST["duree"];
 $preference=$_POST["preference"];
 
-sortir($nom,$adr,$transport,$date,$heure,$minute,$duree, $preference);
+
+
+//sortir($nom,$adr,$transport,$date,$heure,$minute,$duree, $preference);
 
 ?>
+</body>
+</html>
