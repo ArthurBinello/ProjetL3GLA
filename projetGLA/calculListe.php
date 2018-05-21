7dd4f1cc422460c7b09f4adaac8ce7d73aa453f3
Arthur Binello <arthur.binello@gmail.com>
2018-05-22 00:27:28 +0200

    Update calculListe.php

--- projetGLA/calculListe.php
+++ projetGLA/calculListe.php
@@ -1,5 +1,7 @@
 <?php
 
+include 'googleAPI.php';
+
 function prochainElem($liste, $index){
 	$max = 20;
 	if($i+1 >= $max){
@@ -9,6 +11,18 @@ function prochainElem($liste, $index){
 		$i++;
 		return $liste[$i];
 	}
+}
+
+function choixSorties($lat, $lng, $sortie1, $sortie2, $sortie3){
+	$sorties = array();
+	$event1 = decode_json_to_array(nearbysearch($lat, $lng, $sortie1));
+	array_push($sorties, $event1);
+	$event2 = decode_json_to_array(nearbysearch($lat, $lng, $sortie2));
+	array_push($sorties, $event2);
+	$event3 = decode_json_to_array(nearbysearch($lat, $lng, $sortie3));
+	array_push($sorties, $event3);
+
+	return $sorties;
 }
 
 ?>
