3da3c36502cf81690754768d7f036b1e7ca1ca41
Arthur Binello <arthur.binello@gmail.com>
2018-05-22 22:29:36 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -5,15 +5,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sortie à Paris</title>
     <link rel="stylesheet" type="text/css" href="style.css" />
-     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-     <link rel="stylesheet" href="/resources/demos/style.css">
-     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-     <script>
-     $( function() {
-       $( "#datepicker" ).datepicker();
-     } );
-     </script>
+    <link rel="shortcut icon" type="image/png" href="images/logo.jpg"/>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>
+    $( function() {
+      $( "#datepicker" ).datepicker();
+    } );
+    </script>
 </head>
 
 <body id="back">
